d25d7ec9af45ae6ea1a571803b2c767396872181
Charleschua <charleschua19@gmail.com>
2022-08-21 17:41:39 +0800

    update 21 Aug

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6bb643a635d68d225ffdd2dec6ccef64'),
+    },
+  },
+});
